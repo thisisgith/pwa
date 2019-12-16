e05efc3180df21227a11ba01312966c4f4f1520e
teraja <teraja@cisco.com>
2019-12-16 11:41:25 +0530

    intial setup

---
+++ src/app/app-table/app-table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { AppTableDataSource, AppTableItem } from './app-table-datasource';
+
+@Component({
+  selector: 'app-app-table',
+  templateUrl: './app-table.component.html',
+  styleUrls: ['./app-table.component.scss']
+})
+export class AppTableComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatTable, {static: false}) table: MatTable<AppTableItem>;
+  dataSource: AppTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new AppTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
